f65b5b0d186f56986f7ed1d575807539b8d41802
nikhil03a <anikhil03prasanna@gmail.com>
2024-09-23 10:41:56 +0530

    added sponsor

--- src/app/sections/Prize.jsx
+++ src/app/sections/Prize.jsx
@@ -1,66 +1,60 @@
-import React from "react";
+  import React from "react";
 
-export default function Prize() {
-  return (
-    <div className="flex flex-col items-center p-6 cursor-default">
-      <h2 className="text-3xl text-center sm:text-4xl md:text-5xl font-bold bg-gradient-to-t bg-clip-text leading-none text-transparent from-teal-200 to-teal-800/90 mb-12">
-        Prizes
-      </h2>
+  export default function Prize() {
+    return (
+      <div className="flex flex-col items-center p-6 cursor-default">
+        <h2 className="text-3xl text-center sm:text-4xl md:text-5xl font-bold bg-gradient-to-t bg-clip-text leading-none text-transparent from-teal-200 to-teal-800/90 mb-12">
+          Prizes
+        </h2>
 
-      <div className="flex flex-col gap-8 w-full items-center">
-        {/* Prize Pool */}
-        <div className="p-6 text-center bg-gray-800 rounded-xl w-full max-w-lg border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-          <h3 className="text-xl mb-2 text-orange-400">* Prize Pool *</h3>
-          <p className="text-6xl font-bold text-orange-400">₹ 1,70,000</p>
-        </div>
-
-        {/* Prizes */}
-        <div className="flex flex-col md:flex-row gap-4 w-full max-w-6xl">
-          <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl w-full border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-            <h3 className="text-xl mb-2 text-pink-400">* 1st Prize *</h3>
-            <p className="text-5xl font-bold text-pink-400 w-full">₹ 75,000</p>
+        <div className="flex flex-col gap-8 w-full items-center">
+          {/* Prize Pool */}
+          <div className="p-6 text-center bg-gray-800 rounded-xl w-full max-w-lg border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+            <h3 className="text-xl mb-2 text-orange-400">* Prize Pool *</h3>
+            <p className="text-6xl font-bold text-orange-400">₹ 1,70,000</p>
           </div>
 
-          <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl w-full border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-            <h3 className="text-xl mb-2 text-green-400">* 2nd Prize *</h3>
-            <p className="text-5xl font-bold text-green-400 w-full">₹ 50,000</p>
+          {/* Prizes */}
+          <div className="flex flex-col md:flex-row gap-4 w-full max-w-6xl">
+            <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl w-full border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+              <h3 className="text-xl mb-2 text-pink-400">* 1st Prize *</h3>
+              <p className="text-5xl font-bold text-pink-400 w-full">₹ 75,000</p>
+            </div>
+
+            <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl w-full border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+              <h3 className="text-xl mb-2 text-green-400">* 2nd Prize *</h3>
+              <p className="text-5xl font-bold text-green-400 w-full">₹ 50,000</p>
+            </div>
+
+            <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl w-full border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+              <h3 className="text-xl mb-2 text-indigo-400">* 3rd Prize *</h3>
+              <p className="text-5xl font-bold text-indigo-400 w-full">₹ 25,000</p>
+            </div>
           </div>
 
-          <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl w-full border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-            <h3 className="text-xl mb-2 text-indigo-400">* 3rd Prize *</h3>
-            <p className="text-5xl font-bold text-indigo-400 w-full">
-              ₹ 25,000
-            </p>
-          </div>
-        </div>
-
-        {/* Special Awards */}
-        <div className="flex flex-col md:flex-row gap-8 max-w-2xl">
-          <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-            <h3 className="text-xl mb-2 text-pink-400">
-              * The Best Women&apos;s Team *
-            </h3>
-            <p className="text-3xl font-bold text-pink-400">
-              ♀ Women&apos;s Team
-            </p>
-          </div>
-        </div>
-
-        {/* Track Prize */}
-        <div className="flex flex-col md:flex-row gap-8 w-full max-w-4xl">
-          <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-            <h3 className="text-3xl font-bold text-blue-400">
-              * Special Track Prize *
-            </h3>
+          {/* Special Awards */}
+          <div className="flex flex-col md:flex-row gap-8 max-w-2xl">
+            <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+              <h3 className="text-xl mb-2 text-pink-400">
+                * The Best Women&apos;s Team *
+              </h3>
+              <p className="text-3xl font-bold text-pink-400">♀ Women&apos;s Team</p>
+            </div>
           </div>
 
-          <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl border border-gray-600 transition-transform duration-300 ease-in-out hover:scale-105">
-            <h3 className="text-3xl font-bold text-blue-400">
-              * Track Prize *
-            </h3>
+          {/* Track Prize */}
+          <div className="flex flex-col md:flex-row gap-8 w-full max-w-4xl">
+            <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+              <h3 className="text-3xl font-bold text-blue-400">
+                * Special Track Prize *
+              </h3>
+            </div>
+
+            <div className="p-6 text-center flex-1 bg-gray-800 rounded-xl border border-gray-600 shadow-lg hover:shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-105 hover:translate-y-1">
+              <h3 className="text-3xl font-bold text-blue-400">* Track Prize *</h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
--- src/data/sponsorsData.js
+++ src/data/sponsorsData.js
@@ -1,59 +1,19 @@
-import TitleSponsorImage1 from "@/assets/tracks/blockchain.png";
-import TitleSponsorImage2 from "@/assets/tracks/blockchain.png";
-import GoldSponsorImage1 from "@/assets/tracks/blockchain.png";
-import GoldSponsorImage2 from "@/assets/tracks/blockchain.png";
-import SilverSponsorImage1 from "@/assets/tracks/blockchain.png";
-import SilverSponsorImage2 from "@/assets/tracks/blockchain.png";
-import AssociateSponsorImage1 from "@/assets/tracks/blockchain.png";
-import AssociateSponsorImage2 from "@/assets/tracks/blockchain.png";
-import ApparelSponsorImage from "@/assets/tracks/blockchain.png";
-import RefreshmentSponsorImage from "@/assets/tracks/blockchain.png";
+import TitleSponsor from "@/assets/tracks/blockchain.png";
+import PoweredBy from "@/assets/tracks/blockchain.png";
 
 export const sponsorsData = [
   {
-    category: "Title Sponsor",
+    category: "Overall Sponsor",
     color: "text-blue-500",
     images: [
-      { src: TitleSponsorImage1, link: "https://www.example.com/title1" },
-      { src: TitleSponsorImage2, link: "https://www.example.com/title2" }
+      { src: TitleSponsor, link: "https://www.example.com/title1" },
     ],
   },
   {
-    category: "Associate Sponsor",
+    category: "Powered By",
     color: "text-amber-900",
     images: [
-      { src: AssociateSponsorImage1, link: "https://www.example.com/associate1" },
-      { src: AssociateSponsorImage2, link: "https://www.example.com/associate2" }
-    ],
-  },
-  {
-    category: "Gold Sponsor",
-    color: "text-yellow-500",
-    images: [
-      { src: GoldSponsorImage1, link: "https://www.example.com/gold1" },
-      { src: GoldSponsorImage2, link: "https://www.example.com/gold2" }
-    ],
-  },
-  {
-    category: "Silver Sponsor",
-    color: "text-gray-500",
-    images: [
-      { src: SilverSponsorImage1, link: "https://www.example.com/silver1" },
-      { src: SilverSponsorImage2, link: "https://www.example.com/silver2" }
-    ],
-  },
-  {
-    category: "Apparel Sponsor",
-    color: "text-green-500",
-    images: [
-      { src: ApparelSponsorImage, link: "https://www.example.com/apparel" }
-    ],
-  },
-  {
-    category: "Refreshment Sponsor",
-    color: "text-red-500",
-    images: [
-      { src: RefreshmentSponsorImage, link: "https://www.example.com/refreshment" }
+      { src: PoweredBy, link: "https://www.example.com/associate1" },
     ],
   },
 ];
